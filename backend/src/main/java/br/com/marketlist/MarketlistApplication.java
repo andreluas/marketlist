5cbb4bff0f6b524a9b8492ee26db7abb74292348
Andre Almeida <andreluas.k@gmail.com>
2022-01-20 19:50:00 -0300

    initial :tada:

---
+++ backend/src/main/java/br/com/marketlist/MarketlistApplication.java
@@ -0,0 +1,13 @@
+package br.com.marketlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarketlistApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/marketlist/MarketlistApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.marketlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketlistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
