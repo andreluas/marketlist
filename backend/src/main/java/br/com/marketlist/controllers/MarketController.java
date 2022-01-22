db85e15dfdf047c42b53f7aefada8d151ababd49
Andre Almeida <andreluas.k@gmail.com>
2022-01-21 21:46:05 -0300

    market controlelr

--- backend/src/main/java/br/com/marketlist/controllers/MarketController.java
+++ backend/src/main/java/br/com/marketlist/controllers/MarketController.java
@@ -1,12 +1,22 @@
 package br.com.marketlist.controllers;
 
+import java.net.URI;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.marketlist.dto.MarketDTO;
 import br.com.marketlist.services.MarketService;
@@ -22,5 +32,31 @@ public class MarketController {
     public ResponseEntity<Page<MarketDTO>> marketsForCurrentUser(Pageable pageable) {
         Page<MarketDTO> page = service.marketsForCurrentUser(pageable);
         return ResponseEntity.ok().body(page);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<MarketDTO> findById(@PathVariable Long id) {
+
+        MarketDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<MarketDTO> insert(@Valid @RequestBody MarketDTO dto) {
+        MarketDTO newDto = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newDto.getId()).toUri();
+        return ResponseEntity.created(uri).body(newDto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<MarketDTO> update(@PathVariable Long id, @Valid @RequestBody MarketDTO dto) {
+        MarketDTO newDto = service.update(id, dto);
+        return ResponseEntity.ok().body(newDto);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@PathVariable Long id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
